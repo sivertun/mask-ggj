28a5baef3778677baef55ff956f705f07f2019e0
EmilOrv <emilols.eo@gmail.com>
2026-01-30 22:51:04 +0100

    feat: add MainMenuUI

--- Assets/Scripts/UI/UIScreen.cs
+++ Assets/Scripts/UI/UIScreen.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 
-public class UIScreen : MonoBehaviour
+public abstract class UIScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Show()
     {
-        
+        gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hide()
     {
-        
+        gameObject.SetActive(false);
     }
 }
