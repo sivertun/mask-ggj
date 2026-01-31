05f27dda85415c154daf7eba0c5eb577556f2af1
Cryonato <barisduck@gmail.com>
2026-01-31 17:58:59 +0100

    bug: ressolved accidental compilation error

--- Assets/Scripts/Environment/DeathBox.cs
+++ Assets/Scripts/Environment/DeathBox.cs
@@ -1,20 +1,17 @@
-using System.Xml.Serialization;
 using UnityEngine;
-using UnityEngine.LevelManager;
 
 public class DeathBox : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        restartPosition();
+        Debug.Log("DeathBox is active.");
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the object that was hit is the DeathZone
         if (other.CompareTag("Player")) 
         {   
-            other.GetComponent<PlayerController>().resetPosition();
             Debug.Log("Player has died!");
         }
     }
