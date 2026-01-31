9f7e39736e9d6d0fa86a2ccfed9c532b925a9fa1
EmilOrv <emilols.eo@gmail.com>
2026-01-31 20:11:23 +0100

    hotfix: null check currentlycontrollednpc

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -134,6 +134,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        if (!currentlyControlledNPC) return;
         Gizmos.DrawWireCube(currentlyControlledNPC.transform.position - currentlyControlledNPC.transform.up * groundCheckDistance, groundCheckBox);
     }
 }
