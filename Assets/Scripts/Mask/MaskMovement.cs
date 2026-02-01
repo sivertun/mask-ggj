00bba57c92c9b25d307f3c1aee31f66e80f7f365
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2026-02-01 11:36:19 +0100

    Make sound scripts
    Make scripts to play certain sounds.
    Plays sounds as mask is thrown and player is moving.

--- Assets/Scripts/Mask/MaskMovement.cs
+++ Assets/Scripts/Mask/MaskMovement.cs
@@ -10,6 +10,8 @@ public class MaskMovement : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 throwVector;
 
+    private AudioSource[] audioSources;
+
     float catchCooldown = 0f;
 
     bool isAttached = true;
@@ -46,6 +48,7 @@ public class MaskMovement : MonoBehaviour
         throwAction = InputSystem.actions.FindAction("Throw");
         aimAction = InputSystem.actions.FindAction("Aim");
         rb = GetComponent<Rigidbody2D>();
+	audioSources = GetComponents<AudioSource>();
     }
 
     public void Throw(Vector2 velocity, GameObject parent)
@@ -64,6 +67,7 @@ public class MaskMovement : MonoBehaviour
         transform.SetParent(null);
         rb.simulated = true;
         rb.linearVelocity = velocity;
+	audioSources[1].Play(0);
     }
 
     public void Catch(GameObject parent, Transform attachpoint)
@@ -87,6 +91,7 @@ public class MaskMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.simulated = false;
         rb.linearVelocity = Vector2.zero;
+	audioSources[0].Play(0);
     }
 
     void Update()
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -36,6 +36,8 @@ public class PlayerController : MonoBehaviour
         return currentlyControlledNPC;
     }
 
+    private AudioSource[] audioSources;
+
     public void setCurrentlyControlledNPC(GameObject npc)
     {
         if (currentlyControlledNPC)
@@ -85,6 +87,9 @@ public class PlayerController : MonoBehaviour
         {
             currentlyControlledNPC.layer = LayerMask.NameToLayer("Ignore Raycast");
         }
+	      audioSources = GetComponents<AudioSource>();
+	      audioSources[2].Play();
+	      audioSources[2].Pause();
 
         zeroFrictionMaterial = new PhysicsMaterial2D();
         zeroFrictionMaterial.friction = 0f;
@@ -121,6 +126,7 @@ public class PlayerController : MonoBehaviour
             releaseJumpEarly = true;   
             releasedJump = true;
         }
+        // Running logic
         if (holdingRun)
         {
             running = true;
@@ -128,7 +134,21 @@ public class PlayerController : MonoBehaviour
         {
             running = false;
         }
-    }
+	// Sound logic
+	if(holdingRun) {
+		audioSources[2].pitch = 2;
+	} else {
+		audioSources[2].pitch = 1;
+	}
+	if(
+		horizontalInput.x != 0 &&
+		npcGrounded
+	) {
+		audioSources[2].UnPause();
+	} else {
+		audioSources[2].Pause();
+	}
+}
 
     // FixedUpdate is called at a fixed interval and is used for physics operations
     void FixedUpdate()
@@ -177,6 +197,7 @@ public class PlayerController : MonoBehaviour
             jumpOnNextOpportunity = false;
             coyoteCounter = 0;
             velocityToApply.y = jumpPower;
+	    audioSources[3].Play();
         }
         else if (releaseJumpEarly == true && coyoteCounter < 0 && rb.linearVelocity.y > 0)
         {
