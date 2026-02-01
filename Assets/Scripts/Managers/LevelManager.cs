1acedac6585c92313568c26b7d286f744d1a7bf5
EmilOrv <emilols.eo@gmail.com>
2026-02-01 13:41:39 +0100

    add timer UI to each level

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -14,6 +14,9 @@ public class LevelManager : MonoBehaviour
     
     private InputAction restartAction;
 
+    private bool timerActive = false;
+    private float timer = 0f;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -27,17 +30,21 @@ public class LevelManager : MonoBehaviour
 
     private void Start()
     {
+        timer = 0;
         restartAction = InputSystem.actions.FindAction("Restart");
     }
     
     public void LoadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
+        timer = 0f;
+        timerActive = true;
     }
 
     public void RestartLevel()
     {
-        LoadLevel(levels[currentLevelIndex]);;
+        if (currentLevelIndex == -1) return;
+        LoadLevel(levels[currentLevelIndex]);
     }
 
     public void LoadNextLevel()
@@ -51,11 +58,21 @@ public class LevelManager : MonoBehaviour
 
     private void Update()
     {
+        if (timerActive)
+        {
+            timer += Time.deltaTime;
+        }
+        
         if(currentLevelIndex == -1) return;
         
         if (restartAction.WasPressedThisFrame())
         {
             RestartLevel();
         }
+    }
+
+    public float GetTimer()
+    {
+        return timer;
     }
 }
